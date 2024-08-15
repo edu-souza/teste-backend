f6f787296e5197d8e4b5b2ea5f3f28000b9de0b1
Henrique <henrique.urbano2002@hotmail.com>
2024-08-14 21:25:22 -0300

    Adiciona hash com bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,23 +1,31 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsuarioService } from 'src/usuario/usuario.service';
 import { JwtService } from '@nestjs/jwt';
+import { HashService } from 'src/usuario/hash.service';
 
 @Injectable()
 export class AuthService {
   
   constructor(
     private usersService: UsuarioService,
-    private jwtService: JwtService
+    private jwtService: JwtService,
+    private hashService: HashService,
   ) {}
 
   async signIn(email: string, pass: string,): Promise<{ access_token: string }> {
-
     const user = await this.usersService.findByEmail(email);
-    console.log(email, pass);
-    if (user?.senha !== pass) {
-      console.log(user?.senha, pass)
-      throw new UnauthorizedException();
+
+    // Comparando a senha fornecida com a senha armazenada (criptografada)
+    const isPasswordValid = await this.hashService.comparePasswords(pass, user?.senha);
+
+    console.log('Senha fornecida:', pass);
+    console.log('Hash armazenado:', user?.senha);
+    console.log('Resultado da comparação:', isPasswordValid);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Credenciais inválidas');
     }
+
     const payload = { sub: user.id, email: user.email };
     return {
       access_token: await this.jwtService.signAsync(payload),
--- src/usuario/usuario.module.ts
+++ src/usuario/usuario.module.ts
@@ -3,11 +3,12 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuarioEntity } from './usuario.entity';
 import { UsuarioService } from './usuario.service';
 import { UsuarioController } from './usuario.controller';
+import { HashService } from './hash.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsuarioEntity])],
   exports: [UsuarioService],
   controllers: [UsuarioController],
-  providers: [UsuarioService],
+  providers: [UsuarioService, HashService],
 })
 export class UsuarioModule {}
