ac5aae0fe2feb7be52e6cc09b4770405ec2b0c02
Lucas Rosa <lucasdesouzadarosa@hotmail.com>
2024-09-15 10:32:26 -0300

    Implementado o envio de e-mail com o código

--- src/email/mail.module.ts
+++ src/email/mail.module.ts
@@ -1,33 +1,8 @@
 import { Module } from '@nestjs/common';
-import { MailerModule } from '@nestjs-modules/mailer';
-import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter';
-import { MailService } from './mailService';
+import { MailService } from './mail.service';
 
 @Module({
-  imports: [
-    MailerModule.forRoot({
-      transport: {
-        host: 'smtp-mail.outlook.com', // SMTP server do Outlook.com (Hotmail usa o mesmo)
-        port: 587,
-        secure: false, // false para STARTTLS
-        auth: {
-          user: 'lucasdesouzadarosa@hotmail.com', // Seu e-mail @hotmail.com
-          pass: '752486319@@Dog', // Sua senha do e-mail Hotmail
-        },
-      },
-      defaults: {
-        from: '"No Reply" <lucasdesouzadarosa@hotmail.com>', // O e-mail de origem dos e-mails enviados
-      },
-      template: {
-        dir: __dirname + '/templates',
-        adapter: new HandlebarsAdapter(),
-        options: {
-          strict: true,
-        },
-      },
-    }),
-  ],
   providers: [MailService],
-  exports: [MailService],
+  exports: [MailService],  // Exporte o serviço para ser usado em outros módulos
 })
 export class MailModule {}
