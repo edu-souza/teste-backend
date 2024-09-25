e3f9865ccb5b237435f58299f4efc0639a5c5671
Eduardo de Souza <desouzanovesete@gmail.com>
2024-09-24 22:24:18 -0300

    teste db

--- db/data-source.ts
+++ db/data-source.ts
@@ -3,8 +3,8 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 export const dataSourceOptions: DataSourceOptions = {
     type: 'postgres',
     database: 'tribofit',
-    username: 'postgres',
-    password: '0667',
+    username: 'admin',
+    password: '8UEMcJ6SN6S0S36W4VzLkDWvYMTM9qPx',
     port: 5432,
     entities: ['dist/**/*.entity.js'],
     migrations: ['dist/db/migrations/*.js'],
